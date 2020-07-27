37d628be1a4e7ad72f3a85cc3f49dd6d0f4102c4
roylin2000 <63024415+roylin2000@users.noreply.github.com>
2020-07-27 15:37:00 -0400

    Updates

---
+++ precache-manifest.303accc341b8b8c46d2a52e1dcf40cb3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68a01b4cdc9aabff6501812c14402ae2",
+    "url": "/react-chess/index.html"
+  },
+  {
+    "revision": "c2eebe5095fb23492f92",
+    "url": "/react-chess/static/css/main.3525acfe.chunk.css"
+  },
+  {
+    "revision": "d2439713bff321ed9277",
+    "url": "/react-chess/static/js/2.54b845f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-chess/static/js/2.54b845f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2eebe5095fb23492f92",
+    "url": "/react-chess/static/js/main.28a99bae.chunk.js"
+  },
+  {
+    "revision": "7bf79dabeab6448c2b67",
+    "url": "/react-chess/static/js/runtime-main.0cd08013.js"
+  },
+  {
+    "revision": "0ace25a715b0466a02b06a348932e395",
+    "url": "/react-chess/static/media/Bking.0ace25a7.png"
+  },
+  {
+    "revision": "d8b7cc26b290dc97058503fc9db44d02",
+    "url": "/react-chess/static/media/Wbishop.d8b7cc26.png"
+  },
+  {
+    "revision": "5426887ea00cea036d7a9649f93e87de",
+    "url": "/react-chess/static/media/Wking.5426887e.png"
+  },
+  {
+    "revision": "2c6465052b0e65b8bc0ffefa228c461c",
+    "url": "/react-chess/static/media/Wknight.2c646505.png"
+  },
+  {
+    "revision": "880d92c148bddab734f7b32213ee988f",
+    "url": "/react-chess/static/media/Wqueen.880d92c1.png"
+  },
+  {
+    "revision": "3894f2693b49d4913c36eb56997bcf4d",
+    "url": "/react-chess/static/media/Wrook.3894f269.png"
+  }
+]);
